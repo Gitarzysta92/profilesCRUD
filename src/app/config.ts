2830b50495a02b8673371fb60bdf5cb6a623630a
Michał Łukasiewicz <michal@threesixty.com>
2019-08-08 09:32:08 +0200

    Add config fix

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 
-const heroku = 'https://cryptic-tundra-52266.herokuapp.com/';
+const heroku = 'https://cryptic-tundra-52266.herokuapp.com';
 const localhost = 'http://localhost:3000';
 
 export const url = heroku
