1588d51bb33c9bcc7511226fbc477e6dde6bef1e
Michał Łukasiewicz <michal@threesixty.com>
2019-08-27 10:01:54 +0200

    fixes

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,10 +1,9 @@
 
 const heroku = 'https://cryptic-tundra-52266.herokuapp.com';
 const localhost = 'http://localhost:3000';
+const memorium = 'https://memorium.pl'
 
 
 
 
-
-
-export const url = heroku;
+export const url = memorium;
